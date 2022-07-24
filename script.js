797250fe13d5ea959177f61ba196a386662cc29b
Vladislav_Svet <t1nky132@mail.ru>
2022-07-24 16:07:25 +0500

    new design development and new animation

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function getData(){
-  $.getJSON('https://api.nasa.gov/planetary/apod?api_key=wffQwaINBeDDvIWQlcaKYsqu9rDD1lzSwg990G6n', function(data){
-    $('#info__text').html(data.explanation);
-    $('#photo').attr('src', data.hdurl);
-  })
-}
+$(document).ready(function () {
+  function getData(){
+    $.getJSON('https://api.nasa.gov/planetary/apod?api_key=wffQwaINBeDDvIWQlcaKYsqu9rDD1lzSwg990G6n', function(data){
+      $('#info__h2').html(data.title);
+      $('#info__text').html(data.explanation);
+      $('#photo').attr('src', data.hdurl);
 
-getData();
+
+    })
+  }
+  getData();
+
+  setTimeout(function(){
+	   document.body.classList.add('body_visible');
+   },400);
+});
