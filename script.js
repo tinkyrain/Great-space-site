3d0141b651b7a39fa7c177842db40f17cd37771e
Vladislav_Svet <t1nky132@mail.ru>
2022-07-24 16:20:22 +0500

    update: bug fixes

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-$(document).ready(function () {
+window.onload = function () {
   function getData(){
     $.getJSON('https://api.nasa.gov/planetary/apod?api_key=wffQwaINBeDDvIWQlcaKYsqu9rDD1lzSwg990G6n', function(data){
       $('#info__h2').html(data.title);
       $('#info__text').html(data.explanation);
       $('#photo').attr('src', data.hdurl);
-
-
     })
   }
   getData();
 
   setTimeout(function(){
 	   document.body.classList.add('body_visible');
-   },400);
-});
+   },300);
+};
