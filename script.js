45362f38ddaf3a8313b16cf1065ae312b3066418
Vladislav_Svet <t1nky132@mail.ru>
2022-07-25 22:30:59 +0500

    Update: Replacing Jquery with Fetch

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-window.onload = function () {
-  function getData(){
-    $.getJSON('https://api.nasa.gov/planetary/apod?api_key=wffQwaINBeDDvIWQlcaKYsqu9rDD1lzSwg990G6n', function(data){
-      $('#info__h2').html(data.title);
-      $('#info__text').html(data.explanation);
-      $('#photo').attr('src', data.hdurl);
-    })
-  }
-  getData();
+const info_h2 = document.getElementById('info__h2');
+const info_text = document.getElementById('info__text');
+const photo = document.getElementById('photo');
 
-  setTimeout(function(){
-	   document.body.classList.add('body_visible');
-   },300);
-};
+setTimeout(function(){
+   document.body.classList.add('body_visible');
+ },300);
+
+async function getData(){
+  const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=wffQwaINBeDDvIWQlcaKYsqu9rDD1lzSwg990G6n');
+  const data = await response.json();
+  const result = await data;
+
+  info_h2.innerText = data.title;
+  info_text.innerText = data.explanation;
+  photo.src = data.hdurl;
+}
+
+getData();
